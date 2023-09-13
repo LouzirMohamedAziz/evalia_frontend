41005085a6fc5e3977487e78853376431528c859
mohamedbenhamouda <mohamedbh.benhamouda@gmail.com>
2023-09-13 12:18:44 +0100

    Template Choice

---
+++ src/system.config.js
@@ -0,0 +1,11 @@
+'use strict';
+System.config({
+    packages: {
+        app: {
+            format: 'register',
+            defaultExtension: 'js'
+        }
+    }
+});
+System.import('app/main')
+    .then(null, console.error.bind(console));
